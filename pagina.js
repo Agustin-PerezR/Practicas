b9f2e92f634910bcd8963e29e4011426c056412e
JuanFerrin99 <80712434+JuanFerrin99@users.noreply.github.com>
2022-07-04 15:54:22 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -1,13 +1,13 @@
-var os = require("os");
-
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello world")
+  res.send('Ferrin y Perez' + "/** DATE **/" + "_" + "/**  ID **/")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Escuchando el puerto ${port}`)
 })
+
+module.exports = app
