6877226ba7fa8c810d2067612a41027f848c63ae
JuanFerrin99 <80712434+JuanFerrin99@users.noreply.github.com>
2022-07-04 16:02:31 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Ferrin y Perez' + "/** DATE **/" + "_" + "/**  ID **/")
+  res.send('Ferrin y Perez' + "/** DATE **/" + " id:" + "${GITHUB_RUN_ID}")
 })
 
 app.listen(port, () => {
