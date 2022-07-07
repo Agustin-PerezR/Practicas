4312a981b713da5856ed55082cac5d2888a45653
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2022-07-07 14:21:00 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -1,13 +1,13 @@
-var os = require("os");
-
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("Hello world")
+  res.send('v' + "/** DATE **/" + " id:" + "${GITHUB_RUN_ID}")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Estas en el puerto ${port}`)
 })
+
+module.exports = app
