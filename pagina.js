11548135dc4f8da359c94e9bf431c3736a077d2c
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2022-07-07 14:42:32 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Ferrin y Perez' + "/** DATE **/" + " id:" + "${GITHUB_RUN_ID}")
+  res.send("anda")
 })
 
 app.listen(port, () => {
