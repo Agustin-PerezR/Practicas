636920548133922ad9ba9b3cbe8cd8e76ff3bb97
Agustin-PerezR <72412854+Agustin-PerezR@users.noreply.github.com>
2022-07-07 14:44:59 -0300

    Update pagina.js

--- pagina.js
+++ pagina.js
@@ -1,13 +1,15 @@
+var os = requiere('os')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send("anda")
+  res.send('v' + "/** DATE **/" + " id:" + "no anda")
 })
 
 app.listen(port, () => {
-  console.log(`Escuchando el puerto ${port}`)
+  console.log(`Estas en el puerto ${port}`)
 })
 
 module.exports = app
